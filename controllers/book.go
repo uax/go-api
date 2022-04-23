3de62813ae356d2338650b1ca5e70d05cd67f6da
Noah <noah@zixi.net>
2022-04-23 16:37:16 +0800

    add pagination

--- controllers/book.go
+++ controllers/book.go
@@ -9,18 +9,44 @@ import (
 )
 
 func BookList(c *gin.Context) {
-	bookId, _ := strconv.Atoi(c.Param("id")) // c.Param("id")
-	res := DB.Eloquent.Debug().Where("category_id = ? AND status = 1", bookId).Order("id desc").Find(&[]model.Book{})
-	if res.Error != nil {
+	categoryId, _ := strconv.Atoi(c.Param("id"))
+	var p DB.Page
+	if c.ShouldBindQuery(&p) != nil {
 		c.JSON(http.StatusOK, gin.H{
 			"code": 500,
-			"msg":  res.Error.Error(),
-			"data": []model.Book{},
+			"msg":  "invalid params",
 		})
 		return
 	}
-	c.JSON(http.StatusOK, gin.H{
-		"code": 200,
-		"data": res.Value,
+	if p.PageNum <= 0 {
+		p.PageNum = 1
+	}
+	switch {
+	case p.PageSize > 100:
+		p.PageSize = 100
+	case p.PageSize <= 0:
+		p.PageSize = 10
+	}
+
+	var books []model.Book
+	if err := DB.Eloquent.Where("category_id = ? AND status = 1", categoryId).Limit(p.PageSize).Offset((p.PageNum - 1) * p.PageSize).Order("id desc").Find(&books).Error; err != nil {
+		c.JSON(http.StatusOK, gin.H{
+			"code": 500,
+			"msg":  err.Error(),
+		})
+		return
+	}
+
+	var total int
+	DB.Eloquent.Model(&model.Book{}).Where("category_id = ? AND status = 1", categoryId).Count(&total)
+	pages := total / p.PageSize
+	if total%p.PageSize != 0 {
+		pages++
+	}
+	c.JSON(200, gin.H{
+		"code":  200,
+		"data":  books,
+		"total": total,
+		"pages": pages,
 	})
 }
--- database/mysql.go
+++ database/mysql.go
@@ -9,7 +9,6 @@ import (
 var Eloquent *gorm.DB
 
 func init() {
-
 	var err error
 	Eloquent, err = gorm.Open("mysql", "root:526937@tcp(127.0.0.1:3306)/api?charset=utf8&parseTime=True&loc=Local&timeout=10ms")
 	if err != nil {
@@ -18,4 +17,12 @@ func init() {
 	if Eloquent.Error != nil {
 		fmt.Printf("database error: %v", Eloquent.Error)
 	}
+	Eloquent.Debug()
+}
+
+type Page struct {
+	PageNum  int    `form:"page"`
+	PageSize int    `form:"size"`
+	Keyword  string `form:"keyword"`
+	Desc     string `form:"desc"`
 }
